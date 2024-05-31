89d9d1b84eace83e4eaf847aca14a0d16e711d73
Apurv Arya <94875571+Apurv-Arya@users.noreply.github.com>
2024-05-31 19:05:51 +0530

    Update Config.py

--- Config.py
+++ Config.py
@@ -12,9 +12,6 @@ if ENVIRONMENT:
     API_HASH = os.environ.get("API_HASH", "")
     OWNER_ID = os.environ.get("OWNER_ID", 6258709129)
     BOT_TOKEN = os.environ.get("BOT_TOKEN", "")
-    DATABASE_URL = os.environ.get("DATABASE_URL", "")
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")  # Sqlalchemy dropped support for "postgres" name.
-    # https://stackoverflow.com/questions/62688256/sqlalchemy-exc-nosuchmoduleerror-cant-load-plugin-sqlalchemy-dialectspostgre
     MUST_JOIN = os.environ.get("MUST_JOIN", "")
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN.replace("@", "")
@@ -24,8 +21,6 @@ else:
     OWNER_ID = "772384743"
     API_HASH = "d36cfa9250dd3d0d46678b538836ca8b"
     BOT_TOKEN = "7250990840:AAH7hHhSmpb7S7VVP4VXyPYDqz2dsbF4pD0"
-    DATABASE_URL = "mongodb+srv://BinaryBandiT69:BinaryBandiT69@db3.vxnexhs.mongodb.net/?retryWrites=true&w=majority&appName=DB3"
-    DATABASE_URL = DATABASE_URL.replace("postgres", "postgresql")
     MUST_JOIN = "TBots_Father" # must join channel link here
     if MUST_JOIN.startswith("@"):
         MUST_JOIN = MUST_JOIN[1:]
